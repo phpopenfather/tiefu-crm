4b0dc9a68fce401a8f6bd8629e67c16dd547bdc3
X6X8-20120726LT\Administrator <phpopenfather@163.com>
2020-12-04 13:25:06 +0800

    铁斧 crm init

---
+++ runtime/cache/1cf0a5fcd7f1869987c215fea824ef01.php
@@ -0,0 +1,4 @@
+<?php
+//000000259200
+ exit();?>
+a:8:{s:2:"id";i:1;s:8:"username";s:11:"18260068067";s:3:"img";s:0:"";s:9:"thumb_img";s:0:"";s:8:"realname";s:9:"管理员";s:9:"parent_id";i:0;s:14:"structure_name";s:9:"办公室";s:12:"structure_id";i:1;}
---
+++ runtime/cache/2fea04905151dac3a4db6b0b84d2a112.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+s:79:"D:\phpstudy_pro\WWW\tiefucrm/runtime/cache\1cf0a5fcd7f1869987c215fea824ef01.php";
